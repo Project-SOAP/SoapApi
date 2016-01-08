447b446e9cdd60e05d56f8703a5127cc2f0f62f4
hub3rt <hubert.girondiac@gmail.com>
2016-01-08 09:09:22 +0100

    modif socket.js

--- config/sockets.js
+++ config/sockets.js
@@ -44,8 +44,8 @@ module.exports.sockets = {
   // -OR-
   //
 
-   adapter: 'redis',
-   host: '127.0.0.1',
+   //adapter: 'redis',
+   //host: '127.0.0.1',
   // port: 6379,
   // db: 'sails',
   // pass: '<redis auth password>',
