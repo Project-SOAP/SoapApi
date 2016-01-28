e7cea5e2411da3a64372596e91faec9e99f7363c
hub3rt <hubert.girondiac@gmail.com>
2016-01-29 00:26:05 +0100

    modif controleur fonction subscribe

--- api/controllers/AlertMessageController.js
+++ api/controllers/AlertMessageController.js
@@ -9,9 +9,9 @@ module.exports = {
 
     subscribeToAlert : function(req,res){
         if(!req.isSocket)return res.json(401,{err:'is not a socket request'});
-
+        var id = req.param('param')
         console.log('je passe ici dans le controleur des alert messages')
-        AlertMessage.find().exec(
+        AlertMessage.find({AlertMessage:id}).exec(
             function(err,AlertMessages){
                 if(err)return res.error()
                 AlertMessage.subscribe(req, _.pluck(AlertMessages))
@@ -20,4 +20,3 @@ module.exports = {
         )
     }
 };
-
