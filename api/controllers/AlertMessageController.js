7cfd131d314bc4fb887d29c2154cb0881273da27
hub3rt <hubert.girondiac@gmail.com>
2016-01-29 10:02:35 +0100

    test controler alert

--- api/controllers/AlertMessageController.js
+++ api/controllers/AlertMessageController.js
@@ -7,49 +7,67 @@
 
 module.exports = {
 
-    index: function(req,res){
-        var socket = req.socket;
-        var io = sails.io;
+
+
+
+
 
         // emit to all sockets (aka publish)
         // including yourself
-        io.sockets.emit('messageName', {thisIs: 'theMessage'});
+        //io.sockets.emit('messageName', {thisIs: 'theMessage'});
 
         // broadcast to a room (aka publish)
         // excluding yourself, if you're in it
-        socket.broadcast.to('roomName').emit('messageName', {thisIs: 'theMessage'});
+        sendGeneral: function(){
+            socket.broadcast.to('room').emit('messageName', {thisIs: 'theMessage'});
+        },
 
         // emit to a room (aka publish)
         // including yourself
-        io.sockets.in('roomName').emit('messageName', {thisIs: 'theMessage'});
-
+        send: function(){
+            var io = sails.io;
+            io.sockets.in('room').emit('messageName', {thisIs: 'theMessage'});
+        },
         // Join a room (aka subscribe)
         // If you're in the room already, no problem, do nothing
         // If the room doesn't exist yet, it gets created
-        socket.join('roomName');
+        join: function(req,res){
+            var io = sails.io;
+            var socket = req.socket;
 
+            io.socket.join('Alert Message');
+
+            console.log('Je passe dans le join d\'alert');
+            res.json({
+                success: true
+            });
+        },
         // Leave a room (aka unsubscribe)
         // If you're not in the room, no problem, do nothing
         // If the room doesn't exist yet, no problem, do nothing
-        socket.leave('roomName');
+        quit: function(req,res){
+            var socket = req.socket;
+            console.log('je quitte la room');
+            socket.leave('Alert Message');
+        },
 
         // Get all connected sockets in the app
-        sails.io.sockets.clients();
+        //sails.io.sockets.clients();
 
-        // Get all conneted sockets in the room, "roomName"
-        sails.io.sockets.clients('roomName');
+        // Get all conneted sockets in the room, "room"
+        //sails.io.sockets.clients('room');
 
 
-    },
+
     
-    subscribeToAlert : function(req,res){
-        if(!req.isSocket)return res.json(401,{err:'is not a socket request'});
-        var id = req.param('id')
-        console.log('je passe ici dans le controleur des alert messages')
-        AlertMessage.find({AlertMessage:id}).exec(
-            function(err,AlertMessages){
-                if(err)return res.error()
-                AlertMessage.subscribe(req, _.pluck(AlertMessages,'id'))
+    subscribeToAlert : function(req,res) {
+        if (!req.isSocket)return res.json(401, {err: 'is not a socket request'});
+        var id = req.param('id');
+        console.log('je passe ici dans le controleur des alert messages');
+        AlertMessage.find({AlertMessage: id}).exec(
+            function (err, AlertMessages) {
+                if (err)return res.error()
+                AlertMessage.subscribe(req, _.pluck(AlertMessages, 'id'))
                 return res.json(AlertMessages)
             }
         )
