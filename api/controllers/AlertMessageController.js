db2d62f2e1498bb99f8496b7e0d699db84857d39
hub3rt <hubert.girondiac@gmail.com>
2016-01-29 09:15:12 +0100

    modif message controler

--- api/controllers/AlertMessageController.js
+++ api/controllers/AlertMessageController.js
@@ -7,6 +7,57 @@
 
 module.exports = {
 
+    find: function(req, res) {
+        var id = req.param('id');
+        var idShortCut = isShortcut(id);
+
+        if (idShortCut === true) {
+            return next();
+        }
+
+        if (id) {
+            console.log(id);
+            AlertMessage.findOne(id, function(err, alertMessage) {
+
+
+                if (alertMessage === undefined) return res.notFound();
+
+                if (err) return res.json(err);
+                AlertMessage.subscribe(req, _.pluck(AlertMessages,'id'))
+                return res.json(AlertMessages)
+                sails.log(alertMessage)
+                return res.jsonx(alertMessage);
+            });
+        } else {
+            var where = req.param('where');
+
+            if (_.isString(where)) {
+                where = JSON.parse(where);
+            }
+
+            var options = {
+                limit: req.param('limit') || undefined,
+                skip: req.param('skip') || undefined,
+                sort: req.param('sort') || undefined,
+                where: where || undefined
+            };
+
+            AlertMessage.find(options, function(err, alertMessage) {
+
+                if (alertMessage === undefined) return res.notFound();
+                if (err) return next(err);
+
+                return res.json(alertMessage);
+            });
+        }
+
+        function isShortcut(id) {
+            if (id === 'find' || id === 'update' || id === 'create' || id === 'destroy') {
+                return true;
+            }
+        }
+    },
+    
     subscribeToAlert : function(req,res){
         if(!req.isSocket)return res.json(401,{err:'is not a socket request'});
         var id = req.param('id')
@@ -15,7 +66,6 @@ module.exports = {
             function(err,AlertMessages){
                 if(err)return res.error()
                 AlertMessage.subscribe(req, _.pluck(AlertMessages,'id'))
-                sails.sockets.broadcast()
                 return res.json(AlertMessages)
             }
         )
